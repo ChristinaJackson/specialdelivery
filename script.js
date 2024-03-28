5215cad4686dcf8844860498756fca601ebbcc1b
Christina <chirochristina@gmail.com>
2024-03-28 14:16:33 -0700

    delayjs

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let root = document.getElementById('root');
 
 document.addEventListener('DOMContentLoaded', function() {
 const sometext = document.createElement('p');
-sometext.textContent = "This is from a javascript file";
+sometext.textContent = "This is from a github javascript file";
 root.appendChild(sometext);
 });
