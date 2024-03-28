f15568277a819fc264be2636839a3d3d76fb0317
Christina <chirochristina@gmail.com>
2024-03-28 13:45:22 -0700

    somejs

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 console.log('ello')
-let root = document.getElementById('root');
+
 const sometext = document.createElement('p');
-sometext.textContent = "This is from a javascript file";
+sometext.textContent = "This is from a JavaScript file";
+
+const root = document.getElementById('root');
 
 root.appendChild(sometext);
