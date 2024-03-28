1a86e9d1505e8109a6701bcb85320ba2d4411270
Christina <chirochristina@gmail.com>
2024-03-28 14:11:29 -0700

    delayjs

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log('ello');
 let root = document.getElementById('root');
 
+document.addEventListener('DOMContentLoaded', function() {
 const sometext = document.createElement('p');
 sometext.textContent = "This is from a javascript file";
-
 root.appendChild(sometext);
+});
