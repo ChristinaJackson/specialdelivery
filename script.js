ba93dc640fe3f6af8eca38a780725614784ffb84
Christina <chirochristina@gmail.com>
2024-03-28 13:53:48 -0700

    adjustjs

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ console.log('ello')
 const sometext = document.createElement('p');
 sometext.textContent = "This is from a JavaScript file";
 
-const root = document.getElementById('root');
-
-root.appendChild(sometext);
+document.body.appendChild(sometext);
